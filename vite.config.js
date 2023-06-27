2593db970b49a2a1eef3fa8741d9ce0202d0d2cf
Antonio Carlos Virissimo <antonio13050@gmail.com>
2023-06-27 11:27:17 -0700

    Configurar deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    base: "/devinhouse-clamed-v2-lista-de-exercicio-M1S06",
+});
